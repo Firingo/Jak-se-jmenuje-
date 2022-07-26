602d8d9d5ae3e9f6acdb8b8dddc3637ba981e7f0
Firingo <petr.havlin.0605@gmail.com>
2022-07-26 20:58:14 +0200

    3. Lekce - Jednoduchá úloha Jak se jmenuješ?

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Ahoj, jak se jmenuješ?");
+			string a = Console.ReadLine();
+			Console.WriteLine("Jaký jsi?");
+			string b = Console.ReadLine();
+
+			Console.WriteLine(a + " je " + b);
+		}
+	}
+}
